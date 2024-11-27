918a3cbd05d7fb28e83de456b6ef71b984018d9b
liamdoan <lamdoan9898@gmail.com>
2024-11-27 03:21:02 +0200

    layout for booking and map section

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div>
-            <h2>Hello, this is my first NextJS app.</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-3">
+                <div className="bg-yellow-200">
+                    booking
+                </div>
+                <div className="bg-yellow-400 col-span-2">
+                    map
+                </div>
+            </div>
         </div>
     );
 }
