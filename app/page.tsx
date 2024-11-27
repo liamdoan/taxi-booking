c83f112b1dec6058355f806e4832a2f7ec5e86f3
liamdoan <lamdoan9898@gmail.com>
2024-11-27 23:29:57 +0200

    use InputCoordsProvider for page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,23 @@
 import Booking from "@/components/booking-section/Booking";
 import MapLibre from "@/components/map-section/MapLibre"
 import { UserLocationProvider } from "./context/UserLocationContext";
+import { InputCoordsProvider } from "./context/InputCoordsContext";
 
 export default function Home() {
     return (
         <UserLocationProvider>
-            <div>
-                <div className="grid grid-cols-1 lg:grid-cols-3">
-                    <div className="bg-yellow-200">
-                        <Booking />
-                    </div>
-                    <div className="bg-yellow-400 col-span-2">
-                        <MapLibre />
+            <InputCoordsProvider>
+                <div>
+                    <div className="grid grid-cols-1 lg:grid-cols-3">
+                        <div className="bg-yellow-200">
+                            <Booking />
+                        </div>
+                        <div className="bg-yellow-400 col-span-2">
+                            <MapLibre />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </InputCoordsProvider>
         </UserLocationProvider>
     );
 }
