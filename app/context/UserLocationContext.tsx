4d66aac400aa1ea667ebaf7a7b028b7450f6c400
liamdoan <lamdoan9898@gmail.com>
2024-11-27 13:33:01 +0200

    create location context

---
+++ app/context/UserLocationContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+const UserLocationContext = createContext<any>(null);
+
+export const UserLocationProvider: React.FC<{ children: ReactNode }> = ({children}) => {
+    const [userLocation, setUserLocation] = useState<any>();
+
+    useEffect(() => {
+        const getUserLocation = () => {
+            navigator.geolocation.getCurrentPosition((location) => {
+                setUserLocation({
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude
+                });
+            })
+        };
+
+        getUserLocation();
+    }, [])
+
+    return (
+        <UserLocationContext.Provider value={{userLocation, setUserLocation}}>
+            {children}
+        </UserLocationContext.Provider>
+    );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
-import Image from "next/image";
+'use client';
+
 import Booking from "@/components/booking-section/Booking";
-import MapLibre from "@/components/map-section/MapLibre";
+import MapLibre from "@/components/map-section/MapLibre"
+import { UserLocationProvider } from "./context/UserLocationContext";
 
 export default function Home() {
     return (
-        <div>
-            <div className="grid grid-cols-1 lg:grid-cols-3">
-                <div className="bg-yellow-200">
-                    <Booking />
-                </div>
-                <div className="bg-yellow-400 col-span-2">
-                <MapLibre />
+        <UserLocationProvider>
+            <div>
+                <div className="grid grid-cols-1 lg:grid-cols-3">
+                    <div className="bg-yellow-200">
+                        <Booking />
+                    </div>
+                    <div className="bg-yellow-400 col-span-2">
+                        <MapLibre />
+                    </div>
                 </div>
             </div>
-        </div>
+        </UserLocationProvider>
     );
 }
