46c49d3af086855ae1e1d5cfcaeaaec7e53510ff
liamdoan <lamdoan9898@gmail.com>
2024-11-29 03:14:37 +0200

    fix value name

--- app/data/CarOptions.ts
+++ app/data/CarOptions.ts
@@ -3,24 +3,24 @@ export default [
         id: 1,
         category: 'Economy',
         img: '/cars-img/economy-car.avif',
-        charges: 1
+        chargeRate: 1
     },
     {
         id: 2,
         category: 'Comfort',
         img: '/cars-img/comfort-car.avif',
-        charges: 1.5
+        chargeRate: 1.5
     },
     {
         id: 3,
         category: 'Minivan',
         img: '/cars-img/minivan-car.avif',
-        charges: 2
+        chargeRate: 2
     },
     {
         id: 4,
         category: 'Luxury',
         img: '/cars-img/luxurious-car.avif',
-        charges: 4
+        chargeRate: 4
     },
 ]
