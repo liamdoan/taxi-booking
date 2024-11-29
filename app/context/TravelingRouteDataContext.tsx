91a352eb1b6b4b095743f5eff441b0bea76af3b8
liamdoan <lamdoan9898@gmail.com>
2024-11-29 03:06:42 +0200

    fix travelingRouteData context

--- app/context/TravelingRouteDataContext.tsx
+++ app/context/TravelingRouteDataContext.tsx
@@ -3,7 +3,7 @@ import { createContext, ReactNode, useContext, useState } from "react";
 const TravelingRouteDataContext = createContext<any>(null);
 
 export const TravelingRouteDataProvider: React.FC<{children: ReactNode}> = ({children}) => {
-    const [travelingRouteData, setTravelingRouteData] = useState<any>([]);
+    const [travelingRouteData, setTravelingRouteData] = useState<any>();
 
     return (
     <TravelingRouteDataContext.Provider
