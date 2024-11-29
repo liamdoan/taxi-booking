ac64f70a446001094f1e39494feab2d21158114c
liamdoan <lamdoan9898@gmail.com>
2024-11-29 09:48:36 +0200

    create temporary ride history page

---
+++ app/ride-history/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='p-4'>
+        <p className='font-semibold'>This feature is coming soon ...</p>
+    </div>
+  )
+}
+
+export default page
