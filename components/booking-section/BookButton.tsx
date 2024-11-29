eed03077ccbf6a38374ba389de3a25bb84b328fb
liamdoan <lamdoan9898@gmail.com>
2024-11-29 06:29:04 +0200

    disabling logic for button

--- components/booking-section/BookButton.tsx
+++ components/booking-section/BookButton.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { useSelectedCarContext } from '@/app/context/SelectedCarContext';
+import { useHasSelectedAddressContext } from '@/app/context/InputCoordsContext';
+import { useHasFetchTravelingRouteDataSuccessfullyContext } from '@/app/context/TravelingRouteDataContext';
 
 const BookButton = () => {
+    const {selectedCar} = useSelectedCarContext();
+    const {hasSelectedAddress} = useHasSelectedAddressContext();
+    const {hasFetchTravelingRouteDataSuccessfully} = useHasFetchTravelingRouteDataSuccessfullyContext();
+
+    const router = useRouter();
+
+    const isButtonEnabled = selectedCar && hasSelectedAddress && hasFetchTravelingRouteDataSuccessfully;
+
     return (
-        <button className='w-full bg-yellow-400 p-2 mt-10 rounded-md'>
+        <button 
+            className={`
+                w-full p-2 mt-10 rounded-md
+                ${!isButtonEnabled
+                    ? 'bg-gray-300 cursor-not-allowed'
+                    : 'bg-yellow-400'
+                }
+            `}
+            onClick={() => router.push('/payment')}
+            disabled={!isButtonEnabled}
+        >
             Book
         </button>
     )
