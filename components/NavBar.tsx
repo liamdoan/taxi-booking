37dd47d452e2daf14fb8872062e6a1285919ea27
liamdoan <lamdoan9898@gmail.com>
2024-11-29 09:43:59 +0200

    help button navigates to support page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import React from 'react'
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 const NavBar = () => {
+    const router = useRouter();
   return (
     <div className='flex justify-between py-3 px-4 border-b-[2px] shadow-lg'>
         <div className='bg-yellow-400 gap-10'>
@@ -16,9 +20,22 @@ const NavBar = () => {
             />
         </div>
         <div className='flex gap-5 items-center'> {/* add 'hidden sm:' in front to hide elem when screen size is small */}
-            <h2 className='hover:bg-yellow-400 cursor-pointer p-1 transition-all'>home</h2>
-            <h2 className='hover:bg-yellow-400 cursor-pointer p-1 transition-all'>history</h2>
-            <h2 className='hover:bg-yellow-400 cursor-pointer p-1 transition-all'>help</h2>
+            <h2
+                className='hover:bg-yellow-400 cursor-pointer py-1 px-3 rounded-md transition-all'
+            >
+                Home
+            </h2>
+            <h2
+                className='hover:bg-yellow-400 cursor-pointer py-1 px-3 rounded-md transition-all'
+            >
+                History
+            </h2>
+            <h2
+                className='hover:bg-yellow-400 cursor-pointer py-1 px-3 rounded-md transition-all'
+                onClick={() => router.push('/support')}
+            >
+                Help
+            </h2>
         </div>
         <div>
             <SignedOut>
