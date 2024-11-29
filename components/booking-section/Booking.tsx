a5ae0d7c076cb6272a6a9aa24bc9497a22984915
liamdoan <lamdoan9898@gmail.com>
2024-11-29 11:24:12 +0200

    fix layout

--- components/booking-section/Booking.tsx
+++ components/booking-section/Booking.tsx
@@ -11,9 +11,8 @@ const Booking = () => {
     
     return (
         <div className='p-5'>
-            <h2 className='text-[1rem]'>Book Slush taxi</h2>
             <div 
-                className='border-[1px] rounded-md' 
+                className='rounded-md'
                 // style={{height: screenHeight}}
             >
                 <AutoSearchAddress />
