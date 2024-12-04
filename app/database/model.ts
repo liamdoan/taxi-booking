8e3b7b2a55d69094b2352d960abc08553403ac7f
liamdoan <lamdoan9898@gmail.com>
2024-12-05 01:46:12 +0200

    change name

--- app/database/model.ts
+++ app/database/model.ts
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
-const DriveInfoSchema = new Schema({
-    pickup: {
-        type: String,
-        required: true
-    },
-    drop: {
-        type: String,
-        required: true
-    },
-    isRideReceived: {
-        type: Boolean,
-        default: false
-    },
-    isRideFinished: {
-        type: Boolean,
-        default: false
+const RideInfoSchema = new Schema(
+    {
+        pickup: {
+            type: String,
+            required: true
+        },
+        drop: {
+            type: String,
+            required: true
+        },
+        isRideReceived: {
+            type: Boolean,
+            default: false
+        },
+        isRideFinished: {
+            type: Boolean,
+            default: false
+        }
+    }, 
+    {
+        timestamps: true
     }
-}, {
-    timestamps: true
-});
+);
 
-const DriveInfo = mongoose.models.DriveInfo || mongoose.model('DriveInfo', DriveInfoSchema);
+const RideInfo = mongoose.models.RideInfo || mongoose.model('RideInfo', RideInfoSchema);
 
-export default DriveInfo;
+export default RideInfo;
