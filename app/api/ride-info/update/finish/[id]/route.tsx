f5b00704d76a0f5ae52c1282e4585e533734c3c5
liamdoan <lamdoan9898@gmail.com>
2024-12-05 03:05:06 +0200

    toggle finish ride api

---
+++ app/api/ride-info/update/finish/[id]/route.tsx
@@ -0,0 +1,57 @@
+import RideInfo from "@/app/database/model";
+import connectMongoDB from "@/app/database/mongodb";
+import { NextResponse } from "next/server";
+
+export async function PUT(
+    request: any,
+    {params}: {params: {
+        id: string
+    }}
+) {
+    const id = params.id;
+
+    await connectMongoDB();
+
+    try {
+        const ride = await RideInfo.findById(id);
+
+        if (!ride) {
+            return NextResponse.json({
+                message: "No ride found."
+            }, {
+                status: 404
+            });
+        };
+
+        if(!ride.isRideReceived) {
+            return NextResponse.json({
+                message: "Ride must be received before finished!"
+            }, {
+                status: 400
+            })
+        };
+
+        const updatedRideFinished = await RideInfo.findByIdAndUpdate(
+            id,
+            [
+                {
+                    $set: {
+                        isRideFinished: { $not: "$isRideFinished" }
+                    }
+                }
+            ], {
+                new: true // update updatedAt
+            }
+        );
+
+        const message = ride.isRideFinished ? "Ride finished!" : "Ride not finished."
+        return NextResponse.json({
+            message: message,
+            updatedRideFinished
+        }, {
+            status: 200
+        })
+    } catch (error) {
+        console.error(error);
+    }
+};
