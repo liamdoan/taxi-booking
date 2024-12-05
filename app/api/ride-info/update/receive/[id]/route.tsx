6ce828de4a79ee1338d374639e7a94b3bde13b2f
liamdoan <lamdoan9898@gmail.com>
2024-12-05 11:06:49 +0200

    clean

--- app/api/ride-info/update/receive/[id]/route.tsx
+++ app/api/ride-info/update/receive/[id]/route.tsx
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function PUT(request: any, { params }: { params: { id: string } }) {
         const url = new URL(request.url);
-    const id = url.pathname.split("/").pop(); // Extract ID from the URL
+    const id = url.pathname.split("/").pop();
     
     await connectMongoDB();
 
