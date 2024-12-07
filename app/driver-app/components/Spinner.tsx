8131371d39d7c9ebaeb74fb5e2606b87e852c526
liamdoan <lamdoan9898@gmail.com>
2024-12-07 07:32:25 +0200

    pass dynamic value for Spinner

--- app/driver-app/components/Spinner.tsx
+++ app/driver-app/components/Spinner.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const Spinner = () => {
+interface SpinnerProps {
+    width?: number,
+    height?: number
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ width, height}) => {
     return (
         <div className="flex flex-col items-center justify-center">
-            <div className="
-                w-16 h-16 
+            <div className={`
+                w-${width} h-${height} 
                 border-4 border-t-6 border-gray-300 border-t-yellow-500 rounded-full 
                 animate-spin
-            "></div>
+            `}></div>
         </div>
     );
 };
