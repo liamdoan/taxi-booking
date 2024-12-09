0cfafd4b69ae7ecb163162cec610b691df65fbc4
liamdoan <lamdoan9898@gmail.com>
2024-12-09 10:31:57 +0200

    create loading bar to shared component

---
+++ app/shared/components/LoadingBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const LoadingBar: React.FC = () => {
+    return (
+        <div className="relative w-full h-[6px] bg-gray-200 rounded-md">
+            <style jsx>{`
+                @keyframes loading {
+                    0% {
+                        width: 0%;
+                    }
+                    100% {
+                        width: 100%;
+                    }
+                }
+            `}</style>
+            <div
+                className="absolute top-0 left-0 h-full bg-yellow-400 rounded"
+                style={{
+                    animation: 'loading 3s linear infinite'
+                }}
+            ></div>
+        </div>
+    );
+};
+
+export default LoadingBar;
