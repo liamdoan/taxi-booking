db3698474580c9ee549b46cf6e70c2df46e34e8f
liamdoan <lamdoan9898@gmail.com>
2024-12-09 19:51:51 +0200

    add data for Days

---
+++ app/client-app/data/EventDays.ts
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        name: 'Day -1',
+        date: '18 Nov. 2024'
+    },
+    {
+        id: 2,
+        name: 'Day 0',
+        date: '19 Nov. 2024'
+    },
+    {
+        id: 3,
+        name: 'Day 1',
+        date: '20 Nov. 2024'
+    },
+    {
+        id: 4,
+        name: 'Day 2',
+        date: '21 Nov. 2024'
+    }
+];
