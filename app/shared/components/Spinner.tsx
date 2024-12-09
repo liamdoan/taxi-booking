9e0f6242f85e1b13fa691e28a8f7607681d961fd
liamdoan <lamdoan9898@gmail.com>
2024-12-09 11:39:36 +0200

    move spinner to shared component

---
+++ app/shared/components/Spinner.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface SpinnerProps {
+    width: number,
+    height: number
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ width, height }) => {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <div 
+                className={`
+                    border-4 border-t-6 border-gray-300 border-t-yellow-500 rounded-full 
+                    animate-spin
+                `}
+                style={{
+                    width: `${width}px`,
+                    height: `${height}px`
+                }}
+            ></div>
+        </div>
+    );
+};
+
+export default Spinner;
