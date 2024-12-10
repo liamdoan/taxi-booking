ce4ab68f7af61135f2af684a197c27716432ae2d
liamdoan <lamdoan9898@gmail.com>
2024-12-10 17:44:47 +0200

    add guest name, guest amount torequest body

--- app/api/ride-info/send/route.tsx
+++ app/api/ride-info/send/route.tsx
@@ -3,15 +3,17 @@ import connectMongoDB from "@/app/database/mongodb";
 import { NextResponse } from "next/server";
 
 export async function POST(request: any) {
-    const { pickup, drop, pickupDay, pickupDate, pickupTime, distance, estimatedTime } = await request.json();
+    const { guestName, guestAmount, pickup, drop, pickupDay, pickupDate, pickupTime, distance, estimatedTime } = await request.json();
 
     await connectMongoDB();
-    await RideInfo.create({pickup, drop, pickupDay, pickupDate, pickupTime, distance, estimatedTime});
+    await RideInfo.create({guestName, guestAmount, pickup, drop, pickupDay, pickupDate, pickupTime, distance, estimatedTime});
 
     return NextResponse.json(
         {
             message: "drive info sent",
             info: {
+                guestName: guestName,
+                guestAmount: guestAmount,
                 pickup: pickup,
                 drop: drop,
                 pickupDay: pickupDay,
