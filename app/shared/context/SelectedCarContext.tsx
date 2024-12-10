e0746b4b45cdc6f3e99af5eaaa038a102ee3f167
liamdoan <lamdoan9898@gmail.com>
2024-12-10 18:36:49 +0200

    add preferred car value to context

--- app/shared/context/SelectedCarContext.tsx
+++ app/shared/context/SelectedCarContext.tsx
@@ -4,10 +4,11 @@ const SelectedCarContext = createContext<any>(null);
 
 export const SelectedCarProvider: React.FC<{children: ReactNode}> = ({children}) => {
     const [selectedCar, setSelectedCar] = useState<any>(null);
+    const [selectedCategory, setSelectedCategory] = useState<string>('');
 
     return (
         <SelectedCarContext.Provider
-            value={{selectedCar, setSelectedCar}}
+            value={{selectedCar, setSelectedCar, selectedCategory, setSelectedCategory}}
         >
             {children}
         </SelectedCarContext.Provider>
