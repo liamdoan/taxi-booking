6c64811f782bec3b42e523a8914ac071247bbb47
liamdoan <lamdoan9898@gmail.com>
2024-12-10 11:51:22 +0200

    temporarily no need to show pickup date and day

--- app/driver-app/components/RideDetails.tsx
+++ app/driver-app/components/RideDetails.tsx
@@ -8,14 +8,14 @@ const RideDetails: React.FC<{rideInfo: RideInfo}> = ({rideInfo}) => {
                 <p className='col-span-1 font-bold'>Pickup point:</p>
                 <span className='col-span-2'>{rideInfo.pickup}</span>
             </div>
-            <div className='mx-1 my-2 grid grid-cols-3 gap-2'>
+            {/* <div className='mx-1 my-2 grid grid-cols-3 gap-2'>
                 <p className='col-span-1 font-bold'>Pickup day:</p>
                 <span className='col-span-2'>{rideInfo.pickupDay}</span>
             </div>
             <div className='mx-1 my-2 grid grid-cols-3 gap-2'>
                 <p className='col-span-1 font-bold'>Pickup date:</p>
                 <span className='col-span-2'>{rideInfo.pickupDate}</span>
-            </div>
+            </div> */}
             <div className='mx-1 my-2 grid grid-cols-3 gap-2'>
                 <p className='col-span-1 font-bold'>Pickup time:</p>
                 <span className='col-span-2'>{rideInfo.pickupTime}</span>
