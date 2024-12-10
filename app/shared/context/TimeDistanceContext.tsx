54b5d20eb2dfaa22532e987fc9bc35cb311ed359
liamdoan <lamdoan9898@gmail.com>
2024-12-10 05:29:28 +0200

    create context for distance and time

---
+++ app/shared/context/TimeDistanceContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+const TimeDistanceContext = createContext<any>(null);
+
+export const TimeDistanceProvider: React.FC<{children: ReactNode}> = ({children}) => {
+    const [formattedDistance, setFormattedDistance] = useState<string>('');
+    const [formattedTime, setformattedTime] = useState<string>('');
+
+    return(
+        <TimeDistanceContext.Provider
+            value={{
+                formattedDistance,
+                setFormattedDistance,
+                formattedTime,
+                setformattedTime
+            }} 
+        >
+            {children}
+        </TimeDistanceContext.Provider>
+    )
+}
+
+export const useTimeDistanceContext = () => useContext(TimeDistanceContext);
