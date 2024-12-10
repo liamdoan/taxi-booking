a1b3d97d637a4344ce14206aced0500e9c415b2c
liamdoan <lamdoan9898@gmail.com>
2024-12-10 05:00:28 +0200

    update new value for POST request

--- app/api/ride-info/send/route.tsx
+++ app/api/ride-info/send/route.tsx
@@ -3,17 +3,20 @@ import connectMongoDB from "@/app/database/mongodb";
 import { NextResponse } from "next/server";
 
 export async function POST(request: any) {
-    const { pickup, drop } = await request.json();
+    const { pickup, drop, pickupDay, pickupDate, pickupTime } = await request.json();
 
     await connectMongoDB();
-    await RideInfo.create({pickup, drop});
+    await RideInfo.create({pickup, drop, pickupDay, pickupDate, pickupTime});
 
     return NextResponse.json(
         {
             message: "drive info sent",
             info: {
                 pickup: pickup,
-                drop: drop
+                drop: drop,
+                pickupDay: pickupDay,
+                pickupDate: pickupDate,
+                pickupTime: pickupTime
             }
         }, {
             status: 201
