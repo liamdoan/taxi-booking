828a6b4fb47b3d0f3febc63adfbc1c7efc518f82
liamdoan <lamdoan9898@gmail.com>
2024-12-10 19:43:57 +0200

    metadata for client view

--- app/client-app/layout.tsx
+++ app/client-app/layout.tsx
@@ -1,5 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
 import NavBar from "@/app/client-app/components/NavBar";
+
+export const metadata: Metadata = {
+    title: "Splash Cab - Form",
+    description: "Booking form",
+};
 
 export default function RootLayout({
     children,
