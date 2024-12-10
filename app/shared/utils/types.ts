821522ba14dc27e8662cec70e8e2f44a297914b1
liamdoan <lamdoan9898@gmail.com>
2024-12-10 18:08:52 +0200

    update interface

--- app/shared/utils/types.ts
+++ app/shared/utils/types.ts
@@ -1,37 +1,37 @@
 type BoundingBox = [string, string, string, string]; //Tuple
 
 interface Address {
-    "name": string,
-    "house_number": string,
-    "road": string,
-    "neighbourhood": string,
-    "suburb": string,
-    "city": string,
-    "county": string,
-    "state": string,
-    "postcode": string,
-    "country": string,
+    "name": string;
+    "house_number": string;
+    "road": string;
+    "neighbourhood": string;
+    "suburb": string;
+    "city": string;
+    "county": string;
+    "state": string;
+    "postcode": string;
+    "country": string;
     "country_code": string
 };
 
 export interface SuggestedAddressList {
-    "place_id": string,
-    "osm_id": string,
-    "osm_type": string,
-    "licence": string,
-    "lat": string,
-    "lon": string,
-    "boundingbox": BoundingBox,
-    "class": string,
-    "type": string,
-    "display_name": string,
-    "display_place": string,
-    "display_address": string,
+    "place_id": string;
+    "osm_id": string;
+    "osm_type": string;
+    "licence": string;
+    "lat": string;
+    "lon": string;
+    "boundingbox": BoundingBox;
+    "class": string;
+    "type": string;
+    "display_name": string;
+    "display_place": string;
+    "display_address": string;
     "address": Address
 };
 
 export interface UserLocationCoordinates {
-    latitude: number,
+    latitude: number;
     longitude: number
 };
 
@@ -43,7 +43,9 @@ export interface TimePickerDropdownProps {
 };
 
 export interface RideInfo {
-    _id: string,
+    _id: string;
+    guestName: string;
+    guestAmount: string;
     pickup: string;
     pickupDay: string;
     pickupDate: string;
@@ -51,6 +53,7 @@ export interface RideInfo {
     drop: string;
     distance: string;
     estimatedTime: string;
+    preferredCar: string;
     isRideReceived: boolean;
     isRideFinished: boolean
 }
