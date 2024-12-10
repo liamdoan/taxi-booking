2b4c231d7bb652a90448654258b3ea01f3122514
liamdoan <lamdoan9898@gmail.com>
2024-12-10 12:24:18 +0200

    convert 24hr in database to 12hr to show

--- app/driver-app/components/RideDetails.tsx
+++ app/driver-app/components/RideDetails.tsx
@@ -2,6 +2,31 @@ import React from 'react';
 import { RideInfo } from '@/app/shared/utils/types';
 
 const RideDetails: React.FC<{rideInfo: RideInfo}> = ({rideInfo}) => {
+    const convert24To12HourFormat = (time24Hr: any) => {
+        const splitTime = time24Hr.split(':');
+        const hour24 = parseInt(splitTime[0], 10);
+        const minutes = splitTime[1];
+
+        let period = 'AM';
+        let hour12 = hour24;
+
+        if (hour24 === 0) {
+            period = 'AM';
+            hour12 = 12;
+        } else if (hour24 === 12) {
+            period = 'PM';
+            hour12 = 12;
+        } else if (hour24 > 12) {
+            period = 'PM';
+            hour12 = hour24 - 12;
+        } else {
+            period = 'AM';
+        }
+
+        const format12Hr = `${hour12}:${minutes} ${period}`;
+        return format12Hr;
+    };
+
     return (
         <>
             <div className='mx-1 my-2 grid grid-cols-3 gap-2'>
@@ -18,7 +43,7 @@ const RideDetails: React.FC<{rideInfo: RideInfo}> = ({rideInfo}) => {
             </div> */}
             <div className='mx-1 my-2 grid grid-cols-3 gap-2'>
                 <p className='col-span-1 font-bold'>Pickup time:</p>
-                <span className='col-span-2'>{rideInfo.pickupTime}</span>
+                <span className='col-span-2'>{convert24To12HourFormat(rideInfo.pickupTime)}</span>
             </div>
             <div className='mx-1 my-2 grid grid-cols-3 gap-2'>
                 <p className='col-span-1 font-bold'>Dropping point:</p>
