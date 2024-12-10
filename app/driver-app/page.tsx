29024e82d7e2da65fb0110329f00d07616f7cef7
liamdoan <lamdoan9898@gmail.com>
2024-12-10 10:14:15 +0200

    filter rides by pickup day

--- app/driver-app/page.tsx
+++ app/driver-app/page.tsx
@@ -31,31 +31,120 @@ const Home = () => {
         getAllRides();
     }, []);
 
+    // filtered by pickup day
+    const dayMinus1Rides = rideInfos.filter((ride: any) => ride.pickupDay.toLowerCase() === 'day -1');
+    const day0Rides = rideInfos.filter((ride: any) => ride.pickupDay.toLowerCase() === 'day 0');
+    const day1Rides = rideInfos.filter((ride: any) => ride.pickupDay.toLowerCase() === 'day 1');
+    const day2Rides = rideInfos.filter((ride: any) => ride.pickupDay.toLowerCase() === 'day 2');
+
     return (
-        <div className="bg-black flex flex-col justify-center items-center min-h-[100vh] pb-6">
-            <h1 className='text-yellow-500 m-[2rem] text-[2rem]'>List of rides</h1>
-            {loadingInitial 
-                ? <Spinner width={64} height={64}/>
-                : (
-                    <>
-                        {rideInfos.map((rideInfo: any) => (
-                            <div
-                                key={rideInfo._id}
-                                className="
-                                    min-w-[300px] max-w-[600px]
-                                    border-2 border-yellow-400
-                                    rounded-md
-                                    pt-5 px-5 mb-2 mx-5
-                                    text-white
-                                "
-                            >
-                                <RideDetails rideInfo={rideInfo}/>
-                                <RideToggleStatus rideInfo={rideInfo}/>
+        <div className="bg-black min-h-[100vh] p-[3rem]">
+            <h1 className='text-yellow-500 mb-[2rem] text-[2rem]'>List of rides</h1>
+            <div className='flex flex-col'>
+                {loadingInitial
+                    ? <Spinner width={64} height={64}/>
+                    : (
+                        <>
+                            <div className='text-[var(--text-normal)] mb-6'>
+                                <h2 className='my-4'>Day 1 (Wed, 20 Nov. 2024)</h2>
+                                <div className='flex flex-wrap'>
+                                    {dayMinus1Rides.length > 0 ? (
+                                        dayMinus1Rides.map((rideInfo: any) => (
+                                            <div
+                                                key={rideInfo._id}
+                                                className="
+                                                    min-w-[300px] max-w-[600px]
+                                                    border-2 border-yellow-400
+                                                    rounded-md
+                                                    pt-5 px-5 mb-2 mr-5
+                                                    text-[var(--text-normal)]
+                                                "
+                                            >
+                                                <RideDetails rideInfo={rideInfo}/>
+                                                <RideToggleStatus rideInfo={rideInfo}/>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <span className='text-gray-500 italic'>No rides have been booked for this day.</span>
+                                    )}
+                                </div>
                             </div>
-                        ))}
-                    </>
-                )
-            }
+                            <div className='text-[var(--text-normal)] mb-6'>
+                                <h2 className='my-4'>Day 1 (Wed, 20 Nov. 2024)</h2>
+                                <div className='flex flex-wrap'>
+                                    {day0Rides.length > 0 ? (
+                                        day0Rides.map((rideInfo: any) => (
+                                            <div
+                                                key={rideInfo._id}
+                                                className="
+                                                    min-w-[300px] max-w-[600px]
+                                                    border-2 border-yellow-400
+                                                    rounded-md
+                                                    pt-5 px-5 mb-2 mr-5
+                                                    text-[var(--text-normal)]
+                                                "
+                                            >
+                                                <RideDetails rideInfo={rideInfo}/>
+                                                <RideToggleStatus rideInfo={rideInfo}/>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <span className='text-gray-500 italic'>No rides have been booked for this day.</span>
+                                    )}
+                                    </div>
+                            </div>
+                            <div className='text-[var(--text-normal)] mb-6'>
+                                <h2 className='my-4'>Day 1 (Wed, 20 Nov. 2024)</h2>
+                                <div className='flex flex-wrap'>
+                                    {day1Rides.length > 0 ? (
+                                        day1Rides.map((rideInfo: any) => (
+                                            <div
+                                                key={rideInfo._id}
+                                                className="
+                                                    min-w-[300px] max-w-[600px]
+                                                    border-2 border-yellow-400
+                                                    rounded-md
+                                                    pt-5 px-5 mb-2 mr-5
+                                                    text-[var(--text-normal)]
+                                                "
+                                            >
+                                                <RideDetails rideInfo={rideInfo}/>
+                                                <RideToggleStatus rideInfo={rideInfo}/>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <span className='text-gray-500 italic'>No rides have been booked for this day.</span>
+                                    )}
+                                </div>
+                            </div>
+                            <div className='text-[var(--text-normal)] mb-6'>
+                                <h2 className='my-4'>Day 1 (Wed, 20 Nov. 2024)</h2>
+                                <div className='flex flex-wrap'>
+                                    {day2Rides.length > 0 ? (
+                                        day2Rides.map((rideInfo: any) => (
+                                            <div
+                                                key={rideInfo._id}
+                                                className="
+                                                    min-w-[300px] max-w-[600px]
+                                                    border-2 border-yellow-400
+                                                    rounded-md
+                                                    pt-5 px-5 mb-2 mr-5
+                                                    text-[var(--text-normal)]
+                                                "
+                                            >
+                                                <RideDetails rideInfo={rideInfo}/>
+                                                <RideToggleStatus rideInfo={rideInfo}/>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <span className='text-gray-500 italic'>No rides have been booked for this day.</span>
+                                    )}
+                                </div>
+                            </div>
+                        </>
+                    )
+                }
+            </div>
         </div>
     )
 }
