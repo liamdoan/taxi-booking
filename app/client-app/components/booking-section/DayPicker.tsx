7a78e6fb212f13936ab0fb385668d601ca9c5e62
liamdoan <lamdoan9898@gmail.com>
2024-12-10 04:56:21 +0200

    clean dayPicker component

--- app/client-app/components/booking-section/DayPicker.tsx
+++ app/client-app/components/booking-section/DayPicker.tsx
@@ -7,16 +7,14 @@ import { useSelectedDayContext } from '@/app/shared/context/SelectedDayContext';
 
 const DayPicker = () => {
     const {
-        selectedDay,
-        setSelectedDay,
-        selectedDayName,
+        selectedDayId,
+        setSelectedDayId,
         setSelectedDayName,
-        selectedDayDate,
-        setSelectedDayDate
+        setSelectedDayDate,
     } = useSelectedDayContext();
 
     const handleClick = (id: number, name: string, date: string) => {
-        setSelectedDay(id);
+        setSelectedDayId(id);
         setSelectedDayName(name);
         setSelectedDayDate(date);
     };
@@ -25,14 +23,6 @@ const DayPicker = () => {
         if (e.key === 'Enter' || e.key ===' '){
             e.preventDefault();
             handleClick(id, name, date);
-        }
-    }
-
-    const handleSubmit = () => {
-        if (selectedDay) {
-          console.log(`Selected Day: ${selectedDay}, ${selectedDayName}, ${selectedDayDate}`);
-        } else {
-          console.error('No day selected!');
         }
     };
 
@@ -57,19 +47,13 @@ const DayPicker = () => {
                             p-3
                             flex flex-col justify-center items-center
                             cursor-pointer
-                            ${day.id == selectedDay && 'border-yellow-400 border-[3px]'}
+                            ${day.id == selectedDayId && 'border-yellow-400 border-[3px]'}
                         `}
                     >
                         <h1 className='mb-2 font-bold text-yellow-400'>{day.name}</h1>
                         <p className='text-[14px] text-gray-300'>{day.date}</p>
                     </div>
                 ))}
-                 <button
-                    onClick={handleSubmit}
-                    className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-                >
-                    Submit
-                </button>
             </div>
         </div>
     )
