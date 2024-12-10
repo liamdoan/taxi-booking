5efc4cd4b68e037ad832437eacfb28d374826fb1
liamdoan <lamdoan9898@gmail.com>
2024-12-10 14:44:31 +0200

    create context to track if checkbox is ticked

---
+++ app/shared/context/CheckBoxContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+const CheckboxContext = createContext<any>(null);
+
+export const CheckboxProvider: React.FC<{children: ReactNode}> = ({children}) => {
+    const [tickedPickupOptionCheckbox, setTickedPickupOptionCheckbox] = useState('');
+    const [tickedDropOptionCheckbox, setTickedDropOptionCheckbox] = useState('');
+
+    return(
+        <CheckboxContext.Provider
+            value={{
+                tickedPickupOptionCheckbox,
+                setTickedPickupOptionCheckbox,
+                tickedDropOptionCheckbox,
+                setTickedDropOptionCheckbox
+            }}
+        >
+            {children}
+        </CheckboxContext.Provider>
+    )
+}
+
+export const useCheckBoxContext = () => useContext(CheckboxContext);
