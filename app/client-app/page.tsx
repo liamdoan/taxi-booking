e7b1635efc43fadea82a863d5369df339035e6c2
liamdoan <lamdoan9898@gmail.com>
2024-12-10 17:36:22 +0200

    use guest amount context to page

--- app/client-app/page.tsx
+++ app/client-app/page.tsx
@@ -11,40 +11,43 @@ import { SelectedDayProvider } from "../shared/context/SelectedDayContext";
 import { SelectedTimeProvider } from "../shared/context/selectedTimeContext";
 import { TimeDistanceProvider } from "../shared/context/TimeDistanceContext";
 import { CheckboxProvider } from "../shared/context/CheckBoxContext";
+import { GuestAmountProvider } from "../shared/context/GuestAmountContext";
 
 export default function Home() {
     return (
-        <UserLocationProvider>
-            <InputCoordsProvider>
-                <HasSelectedAddressProvider>
-                    <TravelingRouteDataProvider>
-                        <HasFetchTravelingRouteDataSuccessfullyProvider>
-                            <AddressNameProvider>
-                                <SelectedDayProvider>
-                                    <CheckboxProvider>
-                                        <SelectedTimeProvider>
-                                            <TimeDistanceProvider>
-                                                <SelectedCarProvider>
-                                                    <div>
-                                                        <div className="grid grid-cols-1 lg:grid-cols-3 min-h-[100vh]">
-                                                            <div className="bg-[var(--foreground)]">
-                                                                <Booking />
-                                                            </div>
-                                                            <div className="bg-[var(--foreground)] col-span-2">
-                                                                <MapLibre />
+        <GuestAmountProvider>
+            <UserLocationProvider>
+                <InputCoordsProvider>
+                    <HasSelectedAddressProvider>
+                        <TravelingRouteDataProvider>
+                            <HasFetchTravelingRouteDataSuccessfullyProvider>
+                                <AddressNameProvider>
+                                    <SelectedDayProvider>
+                                        <CheckboxProvider>
+                                            <SelectedTimeProvider>
+                                                <TimeDistanceProvider>
+                                                    <SelectedCarProvider>
+                                                        <div>
+                                                            <div className="grid grid-cols-1 lg:grid-cols-3 min-h-[100vh]">
+                                                                <div className="bg-[var(--foreground)]">
+                                                                    <Booking />
+                                                                </div>
+                                                                <div className="bg-[var(--foreground)] col-span-2">
+                                                                    <MapLibre />
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                </SelectedCarProvider>
-                                            </TimeDistanceProvider>
-                                        </SelectedTimeProvider>
-                                    </CheckboxProvider>
-                                </SelectedDayProvider>
-                            </AddressNameProvider>
-                        </HasFetchTravelingRouteDataSuccessfullyProvider>
-                    </TravelingRouteDataProvider>
-                </HasSelectedAddressProvider>
-            </InputCoordsProvider>
-        </UserLocationProvider>
+                                                    </SelectedCarProvider>
+                                                </TimeDistanceProvider>
+                                            </SelectedTimeProvider>
+                                        </CheckboxProvider>
+                                    </SelectedDayProvider>
+                                </AddressNameProvider>
+                            </HasFetchTravelingRouteDataSuccessfullyProvider>
+                        </TravelingRouteDataProvider>
+                    </HasSelectedAddressProvider>
+                </InputCoordsProvider>
+            </UserLocationProvider>
+        </GuestAmountProvider>
     );
 }
