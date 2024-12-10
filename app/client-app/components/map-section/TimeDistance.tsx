badc87e88d80a061d1662a06a6ff51f4a18932e9
liamdoan <lamdoan9898@gmail.com>
2024-12-10 05:42:11 +0200

    update TimeDistance component to use context

--- app/client-app/components/map-section/TimeDistance.tsx
+++ app/client-app/components/map-section/TimeDistance.tsx
@@ -1,26 +1,40 @@
 import React, { useEffect } from 'react'
 import { userTravelingRouteDataContext } from '@/app/shared/context/TravelingRouteDataContext';
+import { useTimeDistanceContext } from '@/app/shared/context/TimeDistanceContext';
 
 const TimeDistance = () => {
     const {travelingRouteData} = userTravelingRouteDataContext();
-    
-    //format distance
-    const distanceInKilometer = travelingRouteData && travelingRouteData.routes[0].distance * 0.001;
-    const roundDistanceInKilometer = Math.round(distanceInKilometer *10)/ 10;
-    const roundDistanceInMeter = Math.round(distanceInKilometer * 1000);
-    const formattedDistance = distanceInKilometer < 1
-        ? roundDistanceInMeter + 'm'
-        : roundDistanceInKilometer + 'km'
+    const {
+        formattedDistance,
+        setFormattedDistance,
+        formattedTime,
+        setformattedTime
+    } = useTimeDistanceContext();
 
-    // Format time
-    const rawDuration = travelingRouteData && travelingRouteData.routes[0].duration;
-    const totalMinutes = Math.floor(rawDuration / 60);
-    const hours = Math.floor(totalMinutes / 60);
-    const minutes = totalMinutes % 60;
-    const minutesFormat = minutes === 1 ? `${minutes}min` : `${minutes}mins`
-    const formattedTime = hours > 0 
-        ? `${hours}h ${minutes}min` 
-        : minutesFormat;
+    useEffect(() => {
+        if (travelingRouteData) {
+            //format distance
+            const distanceInKilometer = travelingRouteData && travelingRouteData.routes[0].distance * 0.001;
+            const roundDistanceInKilometer = Math.round(distanceInKilometer *10)/ 10;
+            const roundDistanceInMeter = Math.round(distanceInKilometer * 1000);
+            const formattedDistance = distanceInKilometer < 1
+                ? roundDistanceInMeter + 'm'
+                : roundDistanceInKilometer + 'km'
+
+            // Format time
+            const rawDuration = travelingRouteData && travelingRouteData.routes[0].duration;
+            const totalMinutes = Math.floor(rawDuration / 60);
+            const hours = Math.floor(totalMinutes / 60);
+            const minutes = totalMinutes % 60;
+            const minutesFormat = minutes === 1 ? `${minutes}min` : `${minutes}mins`
+            const formattedTime = hours > 0 
+                ? `${hours}h ${minutes}min` 
+                : minutesFormat;
+
+            setFormattedDistance(formattedDistance);
+            setformattedTime(formattedTime)
+        }
+    }, [travelingRouteData, formattedDistance, formattedTime])
 
     return (
         <>
