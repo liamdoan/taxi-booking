7d2d0a8fea94e24d3eb83b7ddf33df41944277a5
liamdoan <lamdoan9898@gmail.com>
2024-12-11 09:50:54 +0200

    support contacts

---
+++ app/client-app/data/ContactEmails.ts
@@ -0,0 +1,29 @@
+const ContactEmails = [
+    {
+        id: 1,
+        department: 'General Inquiries',
+        email: 'email@mycompany.com'
+    },
+    {
+        id: 2,
+        department: 'Technical support',
+        email: 'tech-support@mycompany.com'
+    },
+    {
+        id: 3,
+        department: 'Partnership',
+        email: 'partners@mycompany.com'
+    },
+    {
+        id: 4,
+        department: 'Media',
+        email: 'media@mycompany.com'
+    },
+    {
+        id: 5,
+        department: 'Marketing',
+        email: 'marketing@mycompany.com'
+    },
+]
+
+export default ContactEmails;
