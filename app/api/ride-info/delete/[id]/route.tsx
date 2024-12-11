575cfdab62e91f31fe0800408378eb4a08cda234
liamdoan <lamdoan9898@gmail.com>
2024-12-11 06:23:31 +0200

    fix syntax

--- app/api/ride-info/delete/[id]/route.tsx
+++ app/api/ride-info/delete/[id]/route.tsx
@@ -2,15 +2,9 @@ import RideInfo from "@/app/database/model";
 import connectMongoDB from "@/app/database/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-    const { id } = params;
-
-    if (!id) {
-        return NextResponse.json(
-            { message: "ID parameter is missing" },
-            { status: 400 }
-        );
-    }
+export async function DELETE(request: NextRequest) {
+    const url = new URL(request.url);
+    const id = url.pathname.split("/").pop();
 
     await connectMongoDB();
     await RideInfo.findByIdAndDelete(id);
