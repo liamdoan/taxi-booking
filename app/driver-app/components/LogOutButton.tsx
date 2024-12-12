9fd11e5cf17c46694787f28d51215231c6cd5cba
liamdoan <lamdoan9898@gmail.com>
2024-12-12 13:34:32 +0200

    logout button for Driver side
    t

---
+++ app/driver-app/components/LogOutButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useDigitCodeAuthContext } from '@/app/shared/context/DigitCodeAuthContext';
+
+const LogOutButton = () => {
+    const {setIsAuthorizedDriver} = useDigitCodeAuthContext();
+
+    const handleLogout = () => {
+        localStorage.removeItem('accessedTimeDriver');
+        setIsAuthorizedDriver(false);
+    }
+
+    return (
+        <button
+            className='
+                text-red-700
+                border-2 border-red-700 rounded-md
+                py-3 px-5
+                hover:text-white hover:border-red-700 hover:bg-red-700
+                transition-all
+            '
+            onClick={handleLogout}
+        >
+            Log Out
+        </button>
+    )
+}
+
+export default LogOutButton
