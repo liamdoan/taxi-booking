e00abdd4bf91c2f86df05c2b5ea89dadb0dfb838
liamdoan <lamdoan9898@gmail.com>
2024-12-12 13:19:12 +0200

    set ending session, require providing code again

--- app/driver-app/page.tsx
+++ app/driver-app/page.tsx
@@ -1,13 +1,35 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import AllRideInfo from './components/AllRideInfo';
 import { useDigitCodeAuthContext } from '../shared/context/DigitCodeAuthContext';
 import CodeAuthDriverPage from './components/codeAuthDriver/CodeAuthDriverPage';
 
 const Home = () => {
-    const {isAuthorizedDriver} = useDigitCodeAuthContext();
+    const {isAuthorizedDriver, setIsAuthorizedDriver} = useDigitCodeAuthContext();
     const accessCodeDriverSide = process.env.NEXT_PUBLIC_DRIVER_ACCESS_CODE;
+
+    const AUTHORIZATION_TIMEOUT = 60 * 60 * 1000; //1hr
+
+    useEffect(() => {
+        const checkAuthorization = () => {
+            const accessedTimeDriver = localStorage.getItem("accessedTimeDriver");
+
+            if (accessedTimeDriver) {
+                const elapsedTime = Date.now() - Number(accessedTimeDriver);
+
+                if (elapsedTime <= AUTHORIZATION_TIMEOUT) {
+                    setIsAuthorizedDriver(true);
+                } else {
+                    setIsAuthorizedDriver(false);
+                }
+            } else {
+                setIsAuthorizedDriver(false);
+            }
+        };
+
+        checkAuthorization();
+    }, []);
 
     if (!isAuthorizedDriver) {
         return (
