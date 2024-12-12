c0d1343d5800f181fc8772b02dec7bfe1774ce42
liamdoan <lamdoan9898@gmail.com>
2024-12-12 10:12:49 +0200

    use code aiuth provider wrapper in layout

--- app/driver-app/layout.tsx
+++ app/driver-app/layout.tsx
@@ -1,6 +1,7 @@
 // import { ClerkProvider } from "@clerk/nextjs";
 // import NavBar from "@/app/client-app/components/NavBar";
 import type { Metadata } from "next";
+import DigitCodeAuthProviderWrapper from "../shared/components/digitCodeAuth/DigitCodeAuthProviderWrapper";
 
 export const metadata: Metadata = {
     title: "Splash Cab - Drivers",
@@ -14,12 +15,14 @@ export default function RootLayout({
 }>) {
     return (
         // <ClerkProvider>
-        <div>
-            {/* <header> */}
-                {/* <NavBar /> */}
-            {/* </header> */}
-            <main>{children}</main>
-        </div>
+        <DigitCodeAuthProviderWrapper>
+            <div>
+                {/* <header> */}
+                    {/* <NavBar /> */}
+                {/* </header> */}
+                <main>{children}</main>
+            </div>
+        </DigitCodeAuthProviderWrapper>
         // </ClerkProvider>
     );
 }
