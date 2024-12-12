b6d20653dcf91d9ced926b6bed6821500207b75d
liamdoan <lamdoan9898@gmail.com>
2024-12-12 09:32:31 +0200

    create Provider wrapper to be able to use Provider in layout

---
+++ app/shared/components/digitCodeAuth/DigitCodeAuthProviderWrapper.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+import { DigitCodeAuthProvider } from '../../context/DigitCodeAuthContext';
+
+const DigitCodeAuthProviderWrapper: React.FC<{children: ReactNode}> = ({children}) => {
+  return (
+    <DigitCodeAuthProvider>
+        {children}
+    </DigitCodeAuthProvider>
+  )
+}
+
+export default DigitCodeAuthProviderWrapper
